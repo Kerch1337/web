447379ae1378b82fa55d887518dd07c8b1c2f395
Kornenko Kirill <kornenko_k@mail.ru>
2025-02-14 02:51:04 +0300

    Реакт приложение с двумя ссылками

---
+++ armageddon-app/src/pages/Destroyment.jsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/header/Header"
+import styles from "./Destroyment.module.css";
+import ImageDestroyment from "./destroyment.jpg";
+
+export const Destroyment = () => {
+    return <div>
+        <Header />
+        <div className={styles.head}>  Destroyment page</div>
+        <div className={styles.container}>
+            <img src={ImageDestroyment} alt="”ничтожение" />
+        </div>
+    </div>
+}
